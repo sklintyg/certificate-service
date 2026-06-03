94e6d338b1a0ec6aeb8229acdb4b63fdc65c7f90
Daniel Jönsson <daniel.joensson@soprasteria.com>
2026-06-03 14:19:12 +0200

    K1J-2168: Fix spotless

--- app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk3226/elements/v1_1/QuestionNarAktivaBehandlingenAvslutadesV1_1.java
+++ app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk3226/elements/v1_1/QuestionNarAktivaBehandlingenAvslutadesV1_1.java
@@ -77,9 +77,7 @@ public class QuestionNarAktivaBehandlingenAvslutadesV1_1 {
                     .anyMatch(value -> value.codeId().equals(ENDAST_PALLIATIV_FIELD_ID)))
         .mapping(new ElementMapping(QUESTION_PATIENTENS_BEHANDLING_OCH_VARDSITUATION_ID, null))
         .pdfConfiguration(
-            PdfConfigurationDate.builder()
-                .pdfFieldId(PDF_PATIENT_SURVIVAL_DATE_FIELD_ID)
-                .build())
+            PdfConfigurationDate.builder().pdfFieldId(PDF_PATIENT_SURVIVAL_DATE_FIELD_ID).build())
         .build();
   }
 }
