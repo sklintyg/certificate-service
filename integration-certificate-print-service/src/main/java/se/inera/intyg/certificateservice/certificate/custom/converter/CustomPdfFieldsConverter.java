eed551f0113e29afde1936cdb08a8f8e12bd4968
Daniel Jönsson <daniel.joensson@soprasteria.com>
2026-06-11 16:57:54 +0200

    K1J-2189: Restructure CustomPdfFieldsConverter logic

--- integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/custom/converter/CustomPdfFieldsConverter.java
+++ integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/custom/converter/CustomPdfFieldsConverter.java
@@ -21,18 +21,21 @@ package se.inera.intyg.certificateservice.certificate.custom.converter;
 import static se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfigurationUnitContactInformation.UNIT_CONTACT_INFORMATION;
 
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.certificate.custom.dto.CustomPdfFieldDTO;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
+import se.inera.intyg.certificateservice.domain.certificate.model.CertificateMetaData;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueDate;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueUnitContactInformation;
 import se.inera.intyg.certificateservice.domain.certificate.model.Status;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CustomPdfSpecification;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.PdfConfigurationDate;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.PdfFieldId;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.PdfSignature;
 import se.inera.intyg.certificateservice.domain.common.model.PaTitle;
 import se.inera.intyg.certificateservice.domain.common.model.Speciality;
@@ -42,108 +45,120 @@ public class CustomPdfFieldsConverter {
 
   public Map<String, CustomPdfFieldDTO> convert(
       Certificate certificate, CustomPdfSpecification spec) {
-    final var fields = new HashMap<String, CustomPdfFieldDTO>();
-
-    addPatientFields(certificate, spec, fields);
-    addDateElementFields(certificate, fields);
-
-    if (certificate.status() == Status.SIGNED) {
-      addSignatureFields(certificate, spec.signature(), fields);
-    }
-
-    addUnitContactInformation(certificate, spec.signature(), fields);
-
-    return fields;
+    return Stream.of(
+            patientFields(certificate, spec),
+            dateElementFields(certificate),
+            signatureFields(certificate, spec),
+            unitContactInformation(certificate, spec.signature()))
+        .flatMap(map -> map.entrySet().stream())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 
-  private static void addPatientFields(
-      Certificate certificate, CustomPdfSpecification spec, Map<String, CustomPdfFieldDTO> fields) {
+  private static Map<String, CustomPdfFieldDTO> patientFields(
+      Certificate certificate, CustomPdfSpecification spec) {
     final var patientId = certificate.getMetadataForPrint().patient().id().idWithoutDash();
-    spec.patientIdFieldIds()
-        .forEach(fieldId -> fields.put(fieldId.id(), new CustomPdfFieldDTO(patientId)));
+    return spec.patientIdFieldIds().stream()
+        .collect(Collectors.toMap(PdfFieldId::id, fieldId -> new CustomPdfFieldDTO(patientId)));
   }
 
-  private static void addDateElementFields(
-      Certificate certificate, Map<String, CustomPdfFieldDTO> fields) {
-    certificate.certificateModel().elementSpecifications().stream()
+  private static Map<String, CustomPdfFieldDTO> dateElementFields(Certificate certificate) {
+    return certificate.certificateModel().elementSpecifications().stream()
         .flatMap(ElementSpecification::flatten)
         .filter(es -> es.pdfConfiguration() instanceof PdfConfigurationDate)
-        .forEach(
+        .flatMap(
             elementSpec ->
                 certificate
                     .getElementDataById(elementSpec.id())
-                    .ifPresent(
+                    .filter(data -> data.value() instanceof ElementValueDate v && v.date() != null)
+                    .map(
                         data -> {
-                          if (data.value() instanceof ElementValueDate valueDate
-                              && valueDate.date() != null) {
-                            final var config =
-                                (PdfConfigurationDate) elementSpec.pdfConfiguration();
-                            fields.put(
-                                config.pdfFieldId().id(),
-                                new CustomPdfFieldDTO(valueDate.date().toString()));
-                          }
-                        }));
+                          final var config = (PdfConfigurationDate) elementSpec.pdfConfiguration();
+                          final var date = ((ElementValueDate) data.value()).date();
+                          return Map.entry(
+                              config.pdfFieldId().id(), new CustomPdfFieldDTO(date.toString()));
+                        })
+                    .stream())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 
-  private static void addSignatureFields(
-      Certificate certificate, PdfSignature signature, Map<String, CustomPdfFieldDTO> fields) {
+  private static Map<String, CustomPdfFieldDTO> signatureFields(
+      Certificate certificate, CustomPdfSpecification spec) {
+    return certificate.status() == Status.SIGNED
+        ? signatureFields(certificate, spec.signature())
+        : Map.of();
+  }
+
+  private static Map<String, CustomPdfFieldDTO> signatureFields(
+      Certificate certificate, PdfSignature signature) {
     final var metadata = certificate.getMetadataForPrint();
-
-    fields.put(
-        signature.signedDateFieldId().id(),
-        new CustomPdfFieldDTO(certificate.signed().format(DateTimeFormatter.ISO_DATE)));
-    fields.put(
-        signature.signedByNameFieldId().id(),
-        new CustomPdfFieldDTO(metadata.issuer().name().fullName()));
-
-    final var paTitles = metadata.issuer().paTitles();
-    if (paTitles != null) {
-      fields.put(
-          signature.paTitleFieldId().id(),
-          new CustomPdfFieldDTO(
-              paTitles.stream().map(PaTitle::code).collect(Collectors.joining(", "))));
-    }
-
-    final var specialities = metadata.issuer().specialities();
-    if (specialities != null) {
-      fields.put(
-          signature.specialtyFieldId().id(),
-          new CustomPdfFieldDTO(
-              specialities.stream().map(Speciality::value).collect(Collectors.joining(", "))));
-    }
-
-    fields.put(
-        signature.hsaIdFieldId().id(), new CustomPdfFieldDTO(metadata.issuer().hsaId().id()));
-
-    final var workplaceCode = metadata.issuingUnit().workplaceCode();
-    if (workplaceCode != null) {
-      fields.put(
-          signature.workplaceCodeFieldId().id(), new CustomPdfFieldDTO(workplaceCode.code()));
-    }
+    return Stream.concat(
+            requiredSignatureFields(certificate, signature, metadata).entrySet().stream(),
+            optionalSignatureFields(signature, metadata).entrySet().stream())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 
-  private static void addUnitContactInformation(
-      Certificate certificate, PdfSignature signature, Map<String, CustomPdfFieldDTO> fields) {
+  private static Map<String, CustomPdfFieldDTO> requiredSignatureFields(
+      Certificate certificate, PdfSignature signature, CertificateMetaData metadata) {
+    return Map.of(
+        signature.signedDateFieldId().id(),
+        new CustomPdfFieldDTO(certificate.signed().format(DateTimeFormatter.ISO_DATE)),
+        signature.signedByNameFieldId().id(),
+        new CustomPdfFieldDTO(metadata.issuer().name().fullName()),
+        signature.hsaIdFieldId().id(),
+        new CustomPdfFieldDTO(metadata.issuer().hsaId().id()));
+  }
+
+  private static Map<String, CustomPdfFieldDTO> optionalSignatureFields(
+      PdfSignature signature, CertificateMetaData metadata) {
+    return Stream.of(
+            Optional.ofNullable(metadata.issuer().paTitles())
+                .map(
+                    titles ->
+                        Map.entry(
+                            signature.paTitleFieldId().id(),
+                            new CustomPdfFieldDTO(
+                                titles.stream()
+                                    .map(PaTitle::code)
+                                    .collect(Collectors.joining(", "))))),
+            Optional.ofNullable(metadata.issuer().specialities())
+                .map(
+                    specs ->
+                        Map.entry(
+                            signature.specialtyFieldId().id(),
+                            new CustomPdfFieldDTO(
+                                specs.stream()
+                                    .map(Speciality::value)
+                                    .collect(Collectors.joining(", "))))),
+            Optional.ofNullable(metadata.issuingUnit().workplaceCode())
+                .map(
+                    wc ->
+                        Map.entry(
+                            signature.workplaceCodeFieldId().id(),
+                            new CustomPdfFieldDTO(wc.code()))))
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+  }
+
+  private static Map<String, CustomPdfFieldDTO> unitContactInformation(
+      Certificate certificate, PdfSignature signature) {
     final var unitName = certificate.getMetadataForPrint().issuingUnit().name().name();
     final var contactInfo =
         certificate
             .getElementDataById(UNIT_CONTACT_INFORMATION)
-            .map(
-                data -> {
-                  if (data.value() instanceof ElementValueUnitContactInformation unitValue) {
-                    return String.join(
-                        "\n",
-                        unitName,
-                        String.join(
-                            "",
-                            unitValue.address(),
-                            ", ",
-                            String.join(" ", unitValue.zipCode(), unitValue.city())),
-                        String.join(" ", "Telefon:", unitValue.phoneNumber()));
-                  }
-                  return "";
-                })
+            .filter(data -> data.value() instanceof ElementValueUnitContactInformation)
+            .map(data -> buildAddress(unitName, (ElementValueUnitContactInformation) data.value()))
             .orElse("");
-    fields.put(signature.contactInformation().id(), new CustomPdfFieldDTO(contactInfo));
+    return Map.of(signature.contactInformation().id(), new CustomPdfFieldDTO(contactInfo));
+  }
+
+  private static String buildAddress(
+      String unitName, ElementValueUnitContactInformation unitValue) {
+    return String.join(
+        "\n",
+        unitName,
+        String.join(
+            "", unitValue.address(), ", ", String.join(" ", unitValue.zipCode(), unitValue.city())),
+        String.join(" ", "Telefon:", unitValue.phoneNumber()));
   }
 }
