01b5be8cd266e0dceeebaaaf0c10d1cf37e2e2ff
ChrilleSigerhed <christoffer.sigerhed@gmail.com>
2026-06-15 14:33:04 +0200

    K1J-2201: Add comment to default implementation for PdfConfiguration

--- domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/PdfConfiguration.java
+++ domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/PdfConfiguration.java
@@ -22,6 +22,14 @@ import java.util.Optional;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
 
 public interface PdfConfiguration {
+
+  /**
+   * Remove this default implementation when all print logic is moved to CPS
+   *
+   * @param elementSpec
+   * @param certificate
+   * @return
+   */
   default Optional<PdfField> toPdfField(ElementSpecification elementSpec, Certificate certificate) {
     return Optional.empty();
   }
